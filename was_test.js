82178fdce97211ef9d2f0a397503f7b7480b7f2e
Roberto Diaz <robertodiaz381@yahoo.com>
2022-09-15 09:04:45 -0500

    test files

---
+++ was_test.js
@@ -0,0 +1,15 @@
+var expect = chai.expect;
+
+describe('My Deck', function(){
+
+
+    describe('#new Deck', function() {
+        it('Should make a new Deck', function() {
+            var z = new Deck;
+            expect(z = []).to.be.an('array');
+        })
+    })
+})
+            
+
+
